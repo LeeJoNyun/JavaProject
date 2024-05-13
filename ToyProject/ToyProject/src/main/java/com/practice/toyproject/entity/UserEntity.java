d8f2f79acd4001f68f39b4c0188237f9ef1261fb
이준현 <115512931+LeeJoNyun@users.noreply.github.com>
2024-05-13 18:01:32 +0900

    로그인시 jwt 발급
    header에  Authorization의 이름으로 Bearer + 토큰 값으로 반환된다.

--- ToyProject/ToyProject/src/main/java/com/practice/toyproject/entity/UserEntity.java
+++ ToyProject/ToyProject/src/main/java/com/practice/toyproject/entity/UserEntity.java
@@ -7,7 +7,7 @@ import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
-@Entity
+@Entity(name="userentity")
 @Getter
 @Setter
 public class UserEntity {
---
+++ ToyProject/ToyProject/src/main/java/com/practice/toyproject/service/CustomUserDetailsService.java
@@ -0,0 +1,35 @@
+package com.practice.toyproject.service;
+
+import com.practice.toyproject.dto.CustomUserDetails;
+import com.practice.toyproject.entity.UserEntity;
+import com.practice.toyproject.repsitory.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    // DB에 접근하기 위한 초기화
+    public CustomUserDetailsService(UserRepository userRepository) {
+
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        //DB에서 조회
+        UserEntity userData = userRepository.findByUsername(username);
+
+        if (userData != null) {
+            //UserDetails에 담아서 return하면 AutneticationManager가 검증 함
+            return new CustomUserDetails(userData);
+        }
+
+        return null;
+    }
+}
